108a691dba082781e12287c68368be79f93cd133
johndevops1995 <jfrodrizuga@gmail.com>
2023-07-24 11:11:36 -0500

    test commit inicial

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
-const parrafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
+const parrafito = document.getElementsByClassName('.parrafito');
+const pid = document.getElementById('pid');
 const input = document.querySelector('input');
+const img = document.createElement('img');
+img.setAttribute('src', 'https://i.pinimg.com/originals/8b/02/40/8b02409f975c98c89e15bc089e3bd289.gif');
+
+console.log(img);
+
+pid.append(img);
+
+console.log(input.value);
 
 console.log({
     h1,
@@ -11,3 +19,7 @@ console.log({
     pid,
     input,
 });
+
+h1.innerHTML = 'patito <br> feo';
+
+h1.innerText = 'patito <br> feo';
