a0a8012ef759240eeafbd55f2f13cc6ee6fb3753
johndevops1995 <jfrodrizuga@gmail.com>
2023-07-25 08:10:30 -0500

    clase 15

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.getElementsByClassName('.parrafito');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
-const img = document.createElement('img');
-img.setAttribute('src', 'https://i.pinimg.com/originals/8b/02/40/8b02409f975c98c89e15bc089e3bd289.gif');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
 
-console.log(img);
-
-pid.append(img);
-
-console.log(input.value);
-
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
-});
-
-h1.innerHTML = 'patito <br> feo';
-
-h1.innerText = 'patito <br> feo';
+function btnOnClick() {
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
